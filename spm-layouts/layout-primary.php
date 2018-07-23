0697fe2ad6bc77fbe3ef123a920c47fc9ff51118
Julianne Ragland <jbragland@gmail.com>
2018-07-23 17:59:24 -0500

    Simplify flexible content loop

--- spm-layouts/layout-primary.php
+++ spm-layouts/layout-primary.php
@@ -1,46 +1,18 @@
 <?php
-// check if the flexible content field has rows of data
 if( have_rows('primary_fields') ):
-
-     // loop through the rows of data
+    
     while ( have_rows('primary_fields') ) : the_row();
-
-        if( get_row_layout() == 'hero' ):
-
-            get_template_part( 'spm-modules/content', 'hero' );
-
-        elseif( get_row_layout() == 'cta' ):
-
-            get_template_part( 'spm-modules/content', 'cta' );
-
-        elseif( get_row_layout() == 'basic' ):
-
-            get_template_part( 'spm-modules/content', 'basic' );
-
-        elseif( get_row_layout() == 'feed' ):
-
-            get_template_part( 'spm-modules/content', 'feed' );
-
-        elseif( get_row_layout() == 'buttons' ): 
-
-            get_template_part( 'spm-modules/content', 'buttons' );
-
-        elseif( get_row_layout() == 'side_by_side' ): 
-
-            get_template_part( 'spm-modules/content', 'side-by-side' );
+    
+        // Save current layout, replacing ACF's underscores with dashes for WP
+        $layout = str_replace( "_", "-", get_row_layout() );
         
-        elseif( get_row_layout() == 'directory_feed' ): 
-
-            get_template_part( 'spm-modules/content', 'directory' );
-
-        endif;
-
+        get_template_part( 'spm-modules/content', $layout );
+    
     endwhile;
 
 else :
 
-    // no layouts found
+    // No layouts found
 
 endif;
-
 ?>
