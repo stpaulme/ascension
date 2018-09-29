10f67f9376526a2f5f1c38e762ae8ac4f1545972
Julianne Ragland <jbragland@gmail.com>
2018-09-29 15:14:17 -0500

    Color test

--- functions.php
+++ functions.php
@@ -193,6 +193,46 @@ class StarterSite extends TimberSite {
 		$context['footer_secondary'] = new TimberMenu( 'footer_secondary' );
 		$context['options'] = get_fields('option');
 
+		function hex2rgba($color, $opacity = false) {
+ 
+			$default = 'rgb(0,0,0)';
+		 
+			//Return default if no color provided
+			if(empty($color))
+				  return $default; 
+		 
+			//Sanitize $color if "#" is provided 
+			if ($color[0] == '#' ) {
+				$color = substr( $color, 1 );
+			}
+		 
+			//Check if color has 6 or 3 characters and get values
+			if (strlen($color) == 6) {
+					$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+			} elseif ( strlen( $color ) == 3 ) {
+					$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+			} else {
+					return $default;
+			}
+		 
+			//Convert hexadec to rgb
+			$rgb =  array_map('hexdec', $hex);
+		 
+			//Check if opacity is set(rgba or rgb)
+			if($opacity) {
+				if(abs($opacity) > 1)
+					$opacity = 1.0;
+				$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
+			} else {
+				$output = 'rgb('.implode(",",$rgb).')';
+			}
+		 
+			//Return rgb(a) color string
+			return $output;
+		}
+		
+		$context['random_color'] = hex2rgba('#ff013f', 0.5);
+
 		function spm_get_form_id( $form_title ) {
 			$forms = GFAPI::get_forms();
 			$form_id = false;
@@ -211,17 +251,9 @@ class StarterSite extends TimberSite {
 		return $context;
 	}
 
-	function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
-
-	
-
 	function add_to_twig( $twig ) {
 		/* this is where you can add your own functions to twig */
 		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
 
