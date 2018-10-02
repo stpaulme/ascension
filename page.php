44c5f63691d1b7a852d57642a027be3a57e6a6a2
Julianne Ragland <jbragland@gmail.com>
2018-10-02 08:36:51 -0500

    Show post content for Privacy Policy pages

--- page.php
+++ page.php
@@ -24,4 +24,9 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
+
+if ( is_page( 'Privacy Policy' ) ) {
+    $context['title'] = $post->name;
+}
+
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
